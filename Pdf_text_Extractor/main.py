f6e2da01091bb55724c875e6c944fe833494907d
Always4ukittu <91136335+Always4ukittu@users.noreply.github.com>
2022-05-08 09:53:17 +0530

    Update main.py

--- Pdf_text_Extractor/main.py
+++ Pdf_text_Extractor/main.py
@@ -1,1 +1,17 @@
 
+from PyPDF2 import PdfFileReader
+
+reader = PdfFileReader("Chapter-14 Biomolecules.pdf.pdf")
+number_of_pages = reader.numPages
+page = reader.pages[0]
+# print(reader.documentInfo)
+# print(reader.getpage())
+
+for i in range(1,20):
+    str = ''
+    # Extract text from the page
+    text = reader.getPage(i).extractText()
+    str += text
+
+with open('file.txt','w',encoding='utf-8') as f:
+    f.write(str)
